27ac16268ebf66e87a5f5ac96d279d361da46fb1
Andres Hernandez <andres.a.h@outlook.com>
2026-06-19 16:09:01 -0700

    added reference_wrapper to <functional>

--- lib/functional.hpp
+++ lib/functional.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "memory.hpp"
 #include "type_traits.hpp"
 #include "utility.hpp"
 
@@ -18,5 +19,54 @@ constexpr R invoke_r(F&& f, Args&&... args) noexcept(is_nothrow_invocable_r_v<R,
     else
         return detail::invoke_impl(cleo::forward<F>(f), cleo::forward<Args>(args)...);
 }
+
+// reference_wrapper
+namespace detail {
+template<typename T>
+constexpr T& FUN(T& t) noexcept {
+    return t;
+}
+
+template<typename T>
+void FUN(T&&) = delete;
+} // namespace detail
+
+template<typename T>
+class reference_wrapper {
+public:
+    using type = T;
+
+    template<
+        typename U,
+        typename =
+            decltype(detail::FUN<T>(cleo::declval<U>()), cleo::enable_if_t<!cleo::is_same_v<reference_wrapper, cleo::remove_cvref_t<U>>>())>
+    constexpr reference_wrapper(U&& u) noexcept(noexcept(detail::FUN<T>(cleo::forward<U>(u))))
+        : ptr_(cleo::addressof(detail::FUN<T>(cleo::forward<U>(u)))) {
+    }
+
+    reference_wrapper(const reference_wrapper&) noexcept = default;
+
+    reference_wrapper& operator=(const reference_wrapper& x) noexcept = default;
+
+    constexpr operator T&() const noexcept {
+        return *ptr_;
+    }
+
+    constexpr T& get() const noexcept {
+        return *ptr_;
+    }
+
+    template<typename... ArgTypes>
+    constexpr cleo::invoke_result_t<T&, ArgTypes...>
+    operator()(ArgTypes&&... args) const noexcept(cleo::is_nothrow_invocable_v<T&, ArgTypes...>) {
+        return cleo::invoke(get(), cleo::forward<ArgTypes>(args)...);
+    }
+
+private:
+    T* ptr_;
+};
+
+template<typename T>
+reference_wrapper(T&) -> reference_wrapper<T>;
 
 } // namespace cleo
