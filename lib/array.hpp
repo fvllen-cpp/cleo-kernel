06fb044514496c60dd2943415eb1c99ca581b957
Andres Hernandez <andres.a.h@outlook.com>
2026-06-22 22:18:53 -0700

    filled in the cleo::array blanks

--- lib/array.hpp
+++ lib/array.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "cstddef.hpp"
+#include "iterator.hpp"
 #include "type_traits.hpp"
+#include "utility.hpp"
 
 namespace cleo {
 
 template<typename T, cleo::size_t N>
+    requires(N > 0)
 class array {
 public:
     using value_type = T;
@@ -20,93 +23,107 @@ public:
     using reverse_iterator = cleo::reverse_iterator<iterator>;
     using const_reverse_iterator = cleo::reverse_iterator<const_iterator>;
 
-    // constructor
-    array() = default;
-
-    // destructor
-    ~array() = default;
-
     // operator=
-    constexpr reference operator=(const array&) = default;
-    constexpr reference operator=(array&&) = default;
-    constexpr const_reference operator=(const array&) const = default;
-    constexpr const_reference operator=(array&&) const = default;
+    constexpr array& operator=(const array&) = default;
+    constexpr array& operator=(array&&) = default;
 
     // element access
-    [[nodiscard]] constexpr reference at(size_type pos) {
+    [[nodiscard]] constexpr reference at(size_type pos) noexcept {
+        return (*this)[pos];
     }
 
-    [[nodiscard]] constexpr const_reference at(size_type pos) const {
+    [[nodiscard]] constexpr const_reference at(size_type pos) const noexcept {
+        return (*this)[pos];
     }
 
     [[nodiscard]] constexpr reference operator[](size_type pos) {
+        return buffer[pos];
     }
 
     [[nodiscard]] constexpr const_reference operator[](size_type pos) const {
+        return buffer[pos];
     }
 
     [[nodiscard]] constexpr reference front() noexcept {
+        return buffer[0];
     }
 
     [[nodiscard]] constexpr const_reference front() const noexcept {
+        return buffer[0];
     }
 
     [[nodiscard]] constexpr reference back() noexcept {
+        return buffer[N - 1];
     }
 
     [[nodiscard]] constexpr const_reference back() const noexcept {
+        return buffer[N - 1];
     }
 
     [[nodiscard]] constexpr T* data() noexcept {
+        return buffer;
     }
 
     [[nodiscard]] constexpr const T* data() const noexcept {
+        return buffer;
     }
 
     // iterators
     [[nodiscard]] constexpr iterator begin() noexcept {
+        return buffer;
     }
 
     [[nodiscard]] constexpr const_iterator begin() const noexcept {
+        return buffer;
     }
 
     [[nodiscard]] constexpr const_iterator cbegin() const noexcept {
+        return buffer;
     }
 
     [[nodiscard]] constexpr iterator end() noexcept {
+        return buffer + N;
     }
 
     [[nodiscard]] constexpr const_iterator end() const noexcept {
+        return buffer + N;
     }
 
     [[nodiscard]] constexpr const_iterator cend() const noexcept {
+        return buffer + N;
     }
 
     [[nodiscard]] constexpr reverse_iterator rbegin() noexcept {
+        return reverse_iterator(end());
     }
 
     [[nodiscard]] constexpr const_reverse_iterator rbegin() const noexcept {
+        return const_reverse_iterator(end());
     }
 
     [[nodiscard]] constexpr const_reverse_iterator crbegin() const noexcept {
+        return const_reverse_iterator(end());
     }
 
     [[nodiscard]] constexpr reverse_iterator rend() noexcept {
+        return reverse_iterator(begin());
     }
 
     [[nodiscard]] constexpr const_reverse_iterator rend() const noexcept {
+        return const_reverse_iterator(begin());
     }
 
     [[nodiscard]] constexpr const_reverse_iterator crend() const noexcept {
+        return const_reverse_iterator(begin());
     }
 
     // capacity
     [[nodiscard]] constexpr bool empty() const noexcept {
-        return size_ == 0;
+        return N == 0;
     }
 
     [[nodiscard]] constexpr size_type size() const noexcept {
-        return size_;
+        return N;
     }
 
     [[nodiscard]] constexpr size_type max_size() const noexcept {
@@ -115,14 +132,21 @@ public:
 
     // operations
     constexpr void fill(const T& value) {
+        for (size_type i = 0; i < N; ++i) {
+            buffer[i] = value;
+        }
     }
 
-    constexpr void swap(array& other) noexcept {
+    constexpr void swap(array& other) noexcept(cleo::is_nothrow_swappable_v<T>)
+        requires cleo::is_swappable_v<T>
+    {
+        for (size_type i = 0; i < N; ++i) {
+            cleo::swap(buffer[i], other.buffer[i]);
+        }
     }
 
 private:
-    T buffer[N] = {0};
-    cleo::size_t size_ = 0;
+    T buffer[N] = {};
 };
 
 // non-member functions
